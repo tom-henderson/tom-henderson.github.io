6719b95f2188f2f9f3446ce20073488f2373b047
Tom Henderson <tomhenderson@mac.com>
2018-12-03 16:56:52 +1300

    Add font-awesome

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+npm install
+
+cp ./node_modules/jquery/dist/jquery.min.js ./assets/js/jquery.min.js
+cp ./node_modules/fitvids/dist/fitvids.min.js ./assets/js/fitvids.min.js
+
+rm -rf ./assets/fonts/*
+cp ./node_modules/font-awesome/css/font-awesome.min.css ./assets/css/font-awesome.min.css
+cp -r ./node_modules/font-awesome/fonts/ ./assets/fonts/
+
+rm -rf ./_sass/bourbon/*
+cp -r ./node_modules/bourbon/core/ ./_sass/bourbon/
+
+rm -rf ./node_modules
